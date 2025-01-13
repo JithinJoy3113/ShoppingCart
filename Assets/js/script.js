278c060289663c707f3f4490488b22a1e1f34f0d
Jithin Joy <jithinj3113@gmail.com>
2025-01-13 20:34:51 +0530

    Shoppingcart

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -100,6 +100,7 @@ function viewSubButton(ID){
             for (let i = 0; i < data.DATA.length; i++) {
                 let childDiv = document.createElement("div");
                 childDiv.classList.add('subCategoryShowDiv')
+                childDiv.setAttribute('id', data.DATA[i][1]);
                 let innerNameDiv = document.createElement("div");
                 innerNameDiv.innerHTML = data.DATA[i][0]; 
                 let innerButtonDiv = document.createElement("div");
@@ -107,6 +108,7 @@ function viewSubButton(ID){
                 let editImg=document.createElement("img");
                 let editButton=document.createElement("button");
                 editButton.classList.add('subCategoryeditButton');
+                editButton.setAttribute('type', 'button');
                 editButton.setAttribute('value', data.DATA[i][1]);
                 editButton.setAttribute('onClick','editSubcategory(this)')
                 editImg.setAttribute('src', "./Assets/Images/editBtn.png");
@@ -115,12 +117,25 @@ function viewSubButton(ID){
 
                 let dltImg=document.createElement("img");
                 let dltButton=document.createElement("button");
-                dltButton.classList.add('subCategoryeditButton')
-                dltButton.setAttribute('value', data.DATA[i][1])
-                dltButton.setAttribute('onClick','dltSubcategory(this)')
+                dltButton.classList.add('subCategoryeditButton');
+                dltButton.setAttribute('type', 'button');
+                dltButton.setAttribute('value', 'tblSubcategory'+','+ data.DATA[i][1])
+                dltButton.setAttribute('onClick','categoryDeleteButton(this)')
                 dltImg.setAttribute('src', "./Assets/Images/deleteBtn.png");
                 dltButton.append(dltImg);
                 innerButtonDiv.append(dltButton);
+
+                let viewImg=document.createElement("img");
+                let viewButton=document.createElement("button");
+                viewButton.classList.add('subCategoryeditButton');
+                viewButton.setAttribute('type', 'button');
+                viewButton.setAttribute('value', data.DATA[i][1])
+                viewButton.setAttribute('onClick','subcategoryViewButton(this)')
+                viewImg.setAttribute('src', "./Assets/Images/goArrow.png");
+                viewImg.setAttribute('width', '18');
+                viewImg.setAttribute('height', '18');
+                viewButton.append(viewImg);
+                innerButtonDiv.append(viewButton);
 
                 childDiv.append(innerNameDiv);
                 childDiv.append(innerButtonDiv);
@@ -130,32 +145,58 @@ function viewSubButton(ID){
     });
 }
 
+function editSubcategory(ID){
+    let editID = ID.value;
+    $.ajax({
+        url : './Components/shoppingCart.cfc?method=viewSubcategoryEdit',
+        type : 'post',
+        data : {
+            subCategoryId : editID
+        },
+        success : function(response){
+            let data=JSON.parse(response);
+            $('#addSubcategoryHeading').text("Edit Subcategory");
+            $("#addCategory").addClass("disabled");
+            $("#addSubcategoryDiv").css({"display":"flex"});
+            $("#viewSubcategory").css({"display":"none"});
+            $("#displayContent").css({"display":"none"});
+            $('#categoryDropdown').val(data.DATA[0][2]);
+            $('#subCategoryInput').val(data.DATA[0][0]);
+            $('#subCategorySubmit').val('edit' +','+ data.DATA[0][1]);
+        }
+    })
+}
+
 function addSubCategory(ID){
     let viewId = ID.value;
+    $('#addSubcategoryHeading').text("Add Subcategory");
     $("#addCategory").addClass("disabled");
     $("#addSubcategoryDiv").css({"display":"flex"});
     $("#viewSubcategory").css({"display":"none"});
     $("#displayContent").css({"display":"none"});
     $("#subCategorySubmit").val(viewId);
+    $("#addCategoryCloseValue").val(viewId);
 }
 
 function addSubcategorySubmit(ID){
     let categoryId = ID.value;
+    let categoryName =  $("#categoryDropdown").val();
     let subcategoryName =  $("#subCategoryInput").val();
     $.ajax({
         url : './Components/shoppingCart.cfc?method=insertSubcategory',
         type : 'post',
         data : {
             subCategoryName : subcategoryName,
+            categoryName : categoryName,
             categoryID : categoryId
         },
         success : function(response){
             let data = JSON.parse(response);
             if (data){
+                console.log(data)
                 $("#subcategoryError").attr("class","text-success");
                 $("#subcategoryError").text('Subcategory Added');
                 $("#subCategoryInput").val('');
-
                 return true;
             }
             else{
@@ -178,14 +219,16 @@ function addCategoryClose(){
     parentDiv.innerHTML = ''
 }
 
-function addSubCategoryClose(ID){
-    let categoryId=ID.value;
+function addSubCategoryClose(){
+    let parentDiv = document.getElementById("categoryFieldDiv");
+    parentDiv.innerHTML = ''
     $("#addSubcategoryDiv").css({"display":"none"});
     $("#addCategory").addClass("disabled");
     $("#displayContent").css({"display":"none"});
     $("#viewSubcategory").css({"display":"flex"});
     $("#subcategoryError").text('');
-    viewSubButton(categoryId);
+    let categoryId = $("#addCategoryCloseValue").val();
+    viewSubButton(document.getElementById('addCategoryCloseValue'));
 }
 
 function loginValidation(){
@@ -213,5 +256,94 @@ function loginValidation(){
         $('#passwordError').text("");
     } 
     return valid;
+}
+
+
+function categoryDeleteButton(ID){
+    let selectedValue = ID.value;
+    let splitValue = selectedValue.split(",");
+    let tableName = splitValue[0];
+    let deleteId = splitValue[1];
+    $.ajax({
+        url : './Components/shoppingCart.cfc?method=deleteRow',
+        type : 'post',
+        data : {
+            tableName : tableName,
+            deleteId : deleteId
+        },
+        success : function(response){
+            if (response){
+                $('#'+deleteId).remove();
+            }
+        }
+    })
+}
+
+function subcategoryViewButton(ID){
+    let viewID = ID.value;
+    $.ajax({
+        url : './Components/shoppingCart.cfc?method=viewSubcategoryEdit',
+        type : 'post',
+        data : {
+            subCategoryId : viewID
+        },
+        success : function(response){
+            let data = JSON.parse(response)
+            $("#subCategoryHead").text(data.DATA[0][0]);
+            $("#addProductButton").val(data.DATA[0][2]+','+data.DATA[0][3]+','+data.DATA[0][0]);
+            $('#productViewMainDiv').css({'display':'flex'})
+            $("#addCategory").css({"display":"none"});
+            $("#viewSubcategory").css({"display":"none"});
+        }
+    })
+}
+
+function addProduct(ID){
+    let categoryId=ID.value;
+    let splitData=categoryId.split(',');
+    let select = $("#addProductCategorySelect");
+    let selectSub = $("#addProductSubcategorySelect");
+    var selectOption=document.createElement("option");
+    var selectSubOption=document.createElement("option");
+    selectOption.text=splitData[0];
+    selectSubOption.text=splitData[2];
+    select.append(selectOption)
+    selectSub.append(selectSubOption)
+    $.ajax({
+        url : './Components/shoppingCart.cfc?method=viewCategory',
+        type : 'post',
+        success : function(response){
+            let data=JSON.parse(response);
+            for (let i = 0; i < data.DATA.length; i++) {
+                if(data.DATA[i][1] != splitData[0]){
+                var selectOption=document.createElement("option");
+                selectOption.text=data.DATA[i][1];
+                select.append(selectOption);
+                }
+            }
+        }
+    }),
+    $.ajax({
+        url : './Components/shoppingCart.cfc?method=viewSubcategories',
+        type : 'post',
+        data :{
+            categoryId : splitData[1]
+        },
+        success : function(response){
+            let subData=JSON.parse(response);
+            for (let i = 0; i < subData.DATA.length; i++) {
+                if(subData.DATA[i][1] != splitData[1]){
+                var selectSubOption=document.createElement("option");
+                selectSubOption.text=subData.DATA[i][0];
+                selectSub.append(selectSubOption);
+                }
+            }
+        }
+    })
+    $('#productViewMainDiv').css({'display':'none'});
+    $('#addProductModal').css({'display':'flex'});
+    $.ajax({
+        url : './Components/shoppingCart.cfc?method='
+    })
 }
 
--- Components/shoppingCart.cfc
+++ Components/shoppingCart.cfc
@@ -84,14 +84,38 @@
     </cffunction>
 
     <cffunction  name="displayCategory">
+        <cfargument  name="categoryName" default="">
         <cfquery name="local.selectCategory">
             SELECT 
                 fldCategory_ID,
                 fldCategoryName 
             FROM tblCategory
+            WHERE
+                <cfif trim(len(arguments.categoryName))> 
+                        fldCategoryName = < cfqueryparam value = '#arguments.categoryName#' cfsqltype = "cf_sql_varchar" > AND
+                        fldActive = < cfqueryparam value = 1 cfsqltype = "cf_sql_integer" >
+                <cfelse>
+                    fldActive = < cfqueryparam value = 1 cfsqltype = "cf_sql_integer" >
+                </cfif>
         </cfquery>
         <cfreturn local.selectCategory>
     </cffunction>
+
+    <cffunction  name="viewCategory" access="remote" returnformat="json" returnType="any">
+        <cfargument  name="categoryName" default="">
+        <cfset local.jsonData = {}>
+        <cfquery name="local.selectCategory">
+            SELECT 
+                fldCategory_ID,
+                fldCategoryName 
+            FROM tblCategory
+            WHERE
+                fldActive = < cfqueryparam value = 1 cfsqltype = "cf_sql_integer" >
+        </cfquery>
+        <cfset local.jsonData = SerializeJSON(local.selectCategory)>
+        <cfreturn local.jsonData>
+    </cffunction>
+
 
     <cffunction  name="updateCategory">
         <cfargument  name="editId">
@@ -122,18 +146,56 @@
 
     <cffunction  name="insertSubcategory" access="remote" returnFormat="json">
         <cfargument  name="subCategoryName">
+        <cfargument  name="categoryName">
         <cfargument  name="categoryID">
+        <cfset local.subCategoryID = ''>
+        <cfif Find(',', arguments.categoryID) GT 0>
+            <cfset local.idArray = listToArray(arguments.categoryID)>
+            <cfset local.subCategoryID = local.idArray[2]>
+        </cfif>
         <cfset local.result = "">
         <cfquery name="local.fetchSubcategory">
-            SELECT 1
+            SELECT 
+                S.fldSubCategoryName,
+                S.fldSubcategory_ID,
+                S.fldCategoryId,
+                C.fldCategoryName,
+                C.fldCategory_ID
             FROM 
-                tblSubcategory
+                tblSubcategory S
+            LEFT JOIN
+                tblCategory C ON C.fldCategory_ID =  S.fldCategoryId
             WHERE
-                fldSubcategoryName = < cfqueryparam value = '#arguments.subCategoryName#' cfsqltype = "varchar" >
+                <cfif len(local.subCategoryID) GT 0>
+                    S.fldSubcategory_ID = < cfqueryparam value = '#local.subCategoryID #' cfsqltype = "cf_sql_integer" >
+                <cfelse>
+                    S.fldSubcategoryName = < cfqueryparam value = '#arguments.subCategoryName#' cfsqltype = "cf_sql_varchar" > AND
+                    C.fldCategoryName = < cfqueryparam value = '#arguments.categoryName#' cfsqltype = "cf_sql_varchar" >
+                </cfif>
+            GROUP BY 
+                S.fldSubcategoryName,
+                S.fldSubcategory_ID,
+                S.fldCategoryId,
+                C.fldCategoryName,
+                C.fldCategory_ID
         </cfquery>
-        <cfif queryRecordCount(fetchSubcategory)>
+
+        <cfif len(local.subCategoryID) GT 0>
+            <cfset local.category = displayCategory(categoryName = arguments.categoryName)>
+            <cfquery name="local.updateSubcategory">
+                    UPDATE tblSubcategory
+                    SET
+                        fldSubcategoryName = < cfqueryparam value = '#arguments.subCategoryName#' cfsqltype = "cf_sql_varchar" >,
+                        fldCategoryId = < cfqueryparam value = '#local.category.fldCategory_ID#' cfsqltype = "cf_sql_integer" >,
+                        fldUpdatedBy = < cfqueryparam value = '#session.userId#' cfsqltype = "cf_sql_integer" >
+                    WHERE
+                        fldSubcategory_ID = < cfqueryparam value = '#local.fetchSubcategory.fldSubcategory_ID#' cfsqltype = "cf_sql_integer" >
+                </cfquery>
+                <cfset local.result = true>
+        <cfelseif queryRecordCount(local.fetchSubcategory)>
             <cfset local.result = false>
         <cfelse>
+            <cfset local.category = displayCategory(categoryName=arguments.categoryName)>
             <cfquery name="local.insertSubcategory">
                 INSERT INTO tblSubcategory
                     (
@@ -143,7 +205,7 @@
                     )
                 VALUES(
                     < cfqueryparam value = '#arguments.subCategoryName#' cfsqltype = "cf_sql_varchar" >,
-                    < cfqueryparam value = '#arguments.categoryID#' cfsqltype = "cf_sql_varchar" >,
+                    < cfqueryparam value = '#local.category.fldCategory_ID#' cfsqltype = "cf_sql_varchar" >,
                     < cfqueryparam value = '#session.userId#' cfsqltype = "cf_sql_integer" >
                 )
             </cfquery>
@@ -151,6 +213,7 @@
         </cfif>
         <cfreturn local.result>
     </cffunction>
+ 
  
     <cffunction  name="viewSubcategory" returnType = "any" access="remote" returnFormat="json">
         <cfargument  name="categoryId">
@@ -162,20 +225,75 @@
             FROM
                 tblSubcategory
             WHERE
-                fldCategoryId = < cfqueryparam value = '#arguments.categoryID#' cfsqltype = "cf_sql_integer" >
+                fldCategoryId = < cfqueryparam value = '#arguments.categoryID#' cfsqltype = "cf_sql_integer" > AND
+                fldActive = < cfqueryparam value = 1 cfsqltype = "cf_sql_integer" >
         </cfquery>
         <cfset local.jsonData = SerializeJSON(local.fetchSubcategory)>
-        <!--- <cfloop query="local.fetchSubcategory">
-            <cfset local.dataArray = []>
-
-        </cfloop> --->
         <cfreturn local.jsonData >
+    </cffunction>
+
+    <cffunction  name="viewSubcategoryEdit" returnType = "any" access="remote" returnFormat="json">
+        <cfargument  name="subCategoryId">
+        <cfset local.jsonData = {}>
+        <cfquery name="local.fetchSubcategory">
+            SELECT
+                S.fldSubcategoryName,
+                S.fldSubcategory_ID,
+                C.fldCategoryName,
+                C.fldCategory_ID
+            FROM
+                tblSubcategory S
+            LEFT JOIN
+                tblCategory C ON C.fldCategory_ID = S.fldCategoryId
+            WHERE
+                fldSubcategory_ID = < cfqueryparam value = '#arguments.subCategoryId#' cfsqltype = "cf_sql_integer" >
+        </cfquery>
+        <cfset local.jsonData = SerializeJSON(local.fetchSubcategory)>
+        <cfreturn local.jsonData >
+    </cffunction>
+
+    <cffunction  name="viewSubcategories" returnType = "any" access="remote" returnFormat="json">
+    <cfargument  name="categoryId">
+        <cfset local.jsonData = {}>
+        <cfquery name="local.fetchSubcategory">
+            SELECT
+                fldSubcategoryName,
+                fldSubcategory_ID
+            FROM
+                tblSubcategory
+            WHERE
+                fldCategoryId = < cfqueryparam value = '#arguments.categoryId#' cfsqltype = "cf_sql_integer" >
+        </cfquery>
+        <cfset local.jsonData = SerializeJSON(local.fetchSubcategory)>
+        <cfreturn local.jsonData >
+    </cffunction>
+
+    <cffunction  name = "deleteRow" returnType = "boolean" access = "remote">
+        <cfargument  name="tableName">
+        <cfargument  name="deleteId">
+        <cfif arguments.tableName EQ 'tblCategory'>
+            <cfset local.columnName = 'fldCategory_ID'>
+        <cfelse>
+            <cfset local.columnName = 'fldSubcategory_ID'>
+        </cfif>
+        <cfquery name="local.deleteData">
+            UPDATE  
+                #arguments.tablename#
+            SET
+                fldActive = < cfqueryparam value = 0 cfsqltype = "cf_sql_integer" >,
+                fldUpdatedBy = < cfqueryparam value = '#session.userId#' cfsqltype = "cf_sql_integer" >
+            WHERE
+                #local.columnName# = < cfqueryparam value = '#arguments.deleteId#' cfsqltype = "cf_sql_integer" >
+        </cfquery>
+        <cfreturn true>
     </cffunction>
 
     <cffunction  name="userLogout" returnType="boolean" access="remote">
         <cfset structClear(session)>
         <cflocation  url="adminLogin.cfm">
     </cffunction>
+
+
 
 </cfcomponent>
 
